0e97257258c7d2b2790d96447d67648fb8a60868
Gabriel Chittolina <gabrielchittolina1@gmail.com>
2019-10-12 17:44:42 -0300

    Change test file from stackoverflow to user

--- tests/index.ts
+++ tests/index.ts
@@ -1,17 +1,17 @@
-import stackoverflow from '../src/api/services/stackoverflow'
+import user from '../src/api/core/user'
 import { expect } from 'chai'
 import 'mocha'
 import server from '../src/api'
 import * as users from './__mocks__/users.json'
 import User from '../src/api/models/user'
 
-describe('Test stackoverflow module', async function() {
+describe('Test user module', async function() {
   before(async function() {
     await User.insertMany(users)
   })
 
   it('Should list users', async () => {
-    const users = await stackoverflow.listUsers()
+    const users = await user.listUsers()
     expect(users).to.have.lengthOf(users.length)
   })
 
