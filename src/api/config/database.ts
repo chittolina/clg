65e4f69aa5225186c28214b8aa134179faf52d7d
Gabriel Chittolina <gabrielchittolina1@gmail.com>
2019-10-12 18:22:29 -0300

    Better variable name

--- src/api/config/database.ts
+++ src/api/config/database.ts
@@ -2,11 +2,11 @@ import * as mongoose from 'mongoose'
 
 const DB_URI = 'mongodb://localhost:27017/stackoverflow'
 
-const obj = {
+const database = {
   start: (onError: Function) => {
     mongoose.connect(DB_URI)
     mongoose.connection.on('error', err => onError(err))
   },
 }
 
-export default obj
+export default database
